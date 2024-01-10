486ba7a0dc6ba469987b4a6c33de6ee95950181f
cbernard10 <clement.bernard@protonmail.com>
2024-01-10 20:01:08 +0100

    added cors

--- api/index.py
+++ api/index.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI, Request
+from fastapi***REMOVED***middleware***REMOVED***cors import CORSMiddleware
 import sympy as sp
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
 app = FastAPI()
+
+
+app***REMOVED***add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app***REMOVED***get("/api/python")
 def hello_world():
