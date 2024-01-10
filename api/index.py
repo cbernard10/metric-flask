6bba39a10d36c2f3cfb285ef033e0a3c73da1ab1
cbernard10 <clement.bernard@protonmail.com>
2024-01-10 19:55:51 +0100

    added basic functions

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import sympy as sp
 
 app = FastAPI()
@@ -6,3 +6,23 @@ app = FastAPI()
 @app***REMOVED***get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
+
+@app***REMOVED***get("/api/ping")
+def ping():
+    return {"message": "pong"}
+
+@app***REMOVED***get("/api/sympy_test")
+def sympy_test():
+    x = sp***REMOVED***Symbol('x')
+    expr = sp***REMOVED***sin(x**2)
+    derivative = sp***REMOVED***diff(expr, x)
+    return {"expr": str(expr), "derivative": str(derivative)}
+            
+@app***REMOVED***post("/api/metric")
+async def get_body(req: Request):
+    body = await req***REMOVED***json()
+    metric = body['metric']
+    metric = sp***REMOVED***Matrix(metric)
+    metric = metric***REMOVED***reshape(3,3)
+    trace = str(sp***REMOVED***trace(metric))
+    return {"trace": trace}
--- components/MetricContainer.jsx
+++ components/MetricContainer.jsx
@@ -6,39 +6,52 @@ import computeMetricConstants from "***REMOVED******REMOVED***/services/metricSe
 
 function MetricContainer() {
   const [entries, setEntries] = useState(Array(9)***REMOVED***fill(""));
+  const [metricConstants, setMetricConstants] = useState({});
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e***REMOVED***preventDefault();
-    console***REMOVED***log("entries", entries)
-    computeMetricConstants(entries)
+    console***REMOVED***log("entries", entries);
+    const result = await computeMetricConstants(entries);
+    setMetricConstants(result);
   };
 
   return (
-    <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-      <div className="grid grid-cols-3 grid-rows-3 border-2 border-white w-96 h-96">
-        {entries***REMOVED***map((entry, index) => {
+    <div>
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <div className="grid grid-cols-3 grid-rows-3 border-2 border-white w-96 h-96">
+          {entries***REMOVED***map((entry, index) => {
+            return (
+              <input
+                className="border-2 border-white bg-neutral-900 flex justify-center items-center text-center"
+                key={index}
+                type="text"
+                value={entry}
+                onChange={(e) => {
+                  const newEntries = [***REMOVED******REMOVED******REMOVED***entries];
+                  newEntries[index] = e***REMOVED***target***REMOVED***value;
+                  setEntries(newEntries);
+                }}
+              ></input>
+            );
+          })}
+        </div>
+        <button
+          type="submit"
+          className="border-white border-2 px-6 py-2 rounded-lg"
+        >
+          Compute
+        </button>
+      </form>
+      <div>
+        {Object***REMOVED***keys(metricConstants)***REMOVED***map((key) => {
           return (
-            <input
-              className="border-2 border-white bg-neutral-900 flex justify-center items-center text-center"
-              key={index}
-              type="text"
-              value={entry}
-              onChange={(e) => {
-                const newEntries = [***REMOVED******REMOVED******REMOVED***entries];
-                newEntries[index] = e***REMOVED***target***REMOVED***value;
-                setEntries(newEntries);
-              }}
-            ></input>
+            <div key={key}>
+              {key}: {metricConstants[key]}
+            </div>
           );
         })}
       </div>
-      <button
-        type="submit"
-        className="border-white border-2 px-6 py-2 rounded-lg"
-      >
-        Compute
-      </button>
-    </form>
+    </div>
   );
 }
 
