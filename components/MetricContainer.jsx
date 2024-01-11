442e04fd965c86aa21590071555234ab6d062f12
cbernard10 <clement.bernard@protonmail.com>
2024-01-11 17:05:17 +0100

    replaced inputs with textareas and style changes

--- components/MetricContainer.jsx
+++ components/MetricContainer.jsx
@@ -4,6 +4,15 @@ import { useState } from "react";
 
 import get from "***REMOVED******REMOVED***/services/metricService";
 
+function auto_grow(element) {
+  element***REMOVED***style***REMOVED***height = "5px";
+  element***REMOVED***style***REMOVED***height = (element***REMOVED***scrollHeight) + "px";
+}
+
+function focusArea(name) {
+  document***REMOVED***getElementById(name)***REMOVED***focus();
+}
+
 function MetricContainer() {
   const [entries, setEntries] = useState(Array(9)***REMOVED***fill(""));
   const [metricConstants, setMetricConstants] = useState({});
@@ -33,7 +42,6 @@ function MetricContainer() {
 
   return (
     <div className="flex flex-row gap-12 py-72 min-h-screen">
-      
       <div className="flex flex-col gap-6 ">
         <form className="bg-black flex flex-row gap-4 items-center justify-between">
           <label>Coordinates</label>
@@ -41,7 +49,7 @@ function MetricContainer() {
             {coordinates***REMOVED***map((coordinate, index) => {
               return (
                 <input
-                  className="w-16 border-[1px] py-2 border-white bg-neutral-900 flex justify-center items-center text-center font-mono"
+                  className="w-16 border-[1px] py-2 border-neutral-600 bg-neutral-900 flex justify-center items-center text-center font-mono"
                   key={index}
                   type="text"
                   value={coordinate}
@@ -60,23 +68,28 @@ function MetricContainer() {
           className="flex flex-col gap-4 items-center"
           onSubmit={handleSubmit}
         >
-          <div className="grid grid-cols-3 grid-rows-3  w-[500px] h-[500px]">
+          <div className="grid grid-cols-3 grid-rows-3 w-[500px] h-[500px] border-2 border-neutral-600">
             {entries***REMOVED***map((entry, index) => {
               return (
-                <input
+                <div
+                  key={index} onClick={() => focusArea(index)}
                   className="
-                    border-2 border-neutral-600 bg-neutral-900 flex flex-row justify-center 
-                    items-center text-center text-xl font-mono resize-none break-all h-full outline-none focus:bg-violet-950
-                  "
-                  key={index}
-                  type="text"
-                  value={entry}
-                  onChange={(e) => {
-                    const newEntries = [***REMOVED******REMOVED******REMOVED***entries];
-                    newEntries[index] = e***REMOVED***target***REMOVED***value;
-                    setEntries(newEntries);
-                  }}
-                ></input>
+                border-2 border-neutral-600 bg-neutral-900 flex flex-row justify-center 
+                items-center text-center font-mono resize-none break-all h-full outline-none focus:bg-violet-950
+                focus-within:bg-violet-950 overflow-clip hover:cursor-text
+              "
+                >
+                  <textarea className="bg-transparent text-white outline-none text-lg" onInput={(e) => auto_grow(e***REMOVED***target)}
+                    type="text"
+                    id={index}
+                    value={entry}
+                    onChange={(e) => {
+                      const newEntries = [***REMOVED******REMOVED******REMOVED***entries];
+                      newEntries[index] = e***REMOVED***target***REMOVED***value;
+                      setEntries(newEntries);
+                    }}
+                  ></textarea>
+                </div>
               );
             })}
           </div>
@@ -91,7 +104,12 @@ function MetricContainer() {
       </div>
 
       <div className="gap-6 w-[800px] mx-auto flex flex-col">
-        <button className="self-end border-2 border-transparent hover:border-neutral-600 bg-neutral-800 font-mono w-10 h-10" onClick={() => setCollapsed((state) => !state)}>{collapsed ? "+" : "-"}</button>
+        <button
+          className="self-end border-2 border-transparent hover:border-neutral-600 bg-neutral-800 font-mono w-10 h-10"
+          onClick={() => setCollapsed((state) => !state)}
+        >
+          {collapsed ? "+" : "-"}
+        </button>
         {!collapsed && (
           <div className="font-mono text-start">
             <pre className="text-white">
