f751562e06a68e794b00bca70b20a2c76098f740
cbernard10 <clement.bernard@protonmail.com>
2024-01-11 10:35:32 +0100

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "***REMOVED***/globals***REMOVED***css";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +22,10 @@ export default function RootLayout({
       lang="en"
       className={`${GeistSans***REMOVED***variable} ${GeistMono***REMOVED***variable} bg-black text-white`}
     >
-      <body className={`${inter***REMOVED***className}`}>{children}</body>
+      <body className={`${inter***REMOVED***className}`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
