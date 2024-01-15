f91d97be4e9441b7528b573ff2c7529d13bc7cbd
cbernard10 <clement.bernard@protonmail.com>
2024-01-15 19:21:36 +0100

    styling

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,18 +5,37 @@ import { useSession, signIn, signOut } from "next-auth/react";
 export default function Header() {
   const { data: session } = useSession();
 
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
   return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
+    <div className="flex flex-row items-center gap-6 self-end">
+      {session ? (
+        <>
+          Signed in
+          {/* as {session.user.email} */}
+          <br />
+          <button
+            onClick={() => signOut()}
+            className="bg-purple-700 text-white rounded-md p-2"
+          >
+            Sign out
+          </button>
+        </>
+      ) : (
+        <>
+          Not signed in <br />
+          <button
+            onClick={() => signIn()}
+            className="bg-purple-700 text-white rounded-md p-2"
+          >
+            Sign in
+          </button>
+        </>
+      )}
+    </div>
+    //   if (session) {
+    //     return (
+
+    //     );
+    //   }
+    //   return (
   );
 }
