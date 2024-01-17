42d859f6545f671935678b81ada9f99cd9229498
cbernard10 <clement.bernard@protonmail.com>
2024-01-17 18:38:16 +0100

    fix

--- components/ResultLatexView/ResultLatexView.jsx
+++ components/ResultLatexView/ResultLatexView.jsx
@@ -7,15 +7,22 @@ import ChristoffelSymbolsLatexView from "./ChristoffelSymbolsLatexView";
 function ResultLatexView({ metricConstants, coordinates }) {
   return (
     <div className="text-xl flex flex-col gap-6">
-        <MathJax>{metricConstants.trace && `\\(\\mathrm{Tr}(g)=${
-          metricConstants.trace.latex
-        }\\)`}</MathJax>
-      <MathJax>{metricConstants.determinant && `\\(\\mathrm{Det}(g)=${
-        metricConstants.determinant && metricConstants.determinant.latex
-      }\\)`}</MathJax>
-      <MathJax>{metricConstants.inverse && `\\(g^{-1} = ${
-        metricConstants.inverse.latex
-      }\\)`}</MathJax>
+      {/* {`\\(\\frac{dg}{d${coordinate}} = ${partial_derivatives[coordinate]}\\)`} */}
+
+      <MathJax>
+        {metricConstants.trace &&
+          `\\(\\mathrm{Tr}\\left(g\\right) = ${metricConstants.trace.latex}\\)`}
+      </MathJax>
+      <MathJax>
+        {metricConstants.determinant &&
+          `\\(\\mathrm{Det}\\left(g\\right) = ${
+            metricConstants.determinant && metricConstants.determinant.latex
+          }\\)`}
+      </MathJax>
+      <MathJax>
+        {metricConstants.inverse &&
+          `\\(g^{-1} = ${metricConstants.inverse.latex}\\)`}
+      </MathJax>
 
       {metricConstants.partial_derivatives && (
         <PartialDerivativesLatexView
