e4c057f60a73b8c9b0eedba6d856fc92709feac7
cbernard10 <clement.bernard@protonmail.com>
2024-01-17 18:43:20 +0100

    fix

--- components/ResultLatexView/ResultLatexView.jsx
+++ components/ResultLatexView/ResultLatexView.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { MathJax, MathJaxContext } from "better-react-mathjax";
 import PartialDerivativesLatexView from "./PartialDerivativesLatexView";
-
 import ChristoffelSymbolsLatexView from "./ChristoffelSymbolsLatexView";
+import TraceLatexView from "./TraceLatexView";
 
 function ResultLatexView({ metricConstants, coordinates }) {
   return (
     <div className="text-xl flex flex-col gap-6">
       {/* {`\\(\\frac{dg}{d${coordinate}} = ${partial_derivatives[coordinate]}\\)`} */}
 
-      <MathJax>
+      {/* <MathJax>
         {metricConstants.trace &&
           `\\(\\mathrm{Tr}\\left(g\\right) = ${metricConstants.trace.latex}\\)`}
       </MathJax>
+       */}
+      {metricConstants.trace && (
+        <TraceLatexView trace={metricConstants.trace.latex} />
+      )}
       <MathJax>
         {metricConstants.determinant &&
           `\\(\\mathrm{Det}\\left(g\\right) = ${
