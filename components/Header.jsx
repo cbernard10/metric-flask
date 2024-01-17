97f772ac123426c7b06c25eb7f99f89c459894cc
cbernard10 <clement.bernard@protonmail.com>
2024-01-17 01:57:17 +0100

    header update

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,35 +11,48 @@ export default function Header() {
     <div
       className="
     flex flex-row items-center gap-6 self-end bg-gradient-to-b 
-    from-neutral-900 to-black w-full h-full p-6 border-b-2 border-b-neutral-950 justify-end"
+    from-neutral-900 to-black w-full h-full px-6 py-6 border-b-2 border-b-neutral-950 justify-end"
     >
       {session ? (
         <>
-          <div className="flex flex-row items-center gap-6">
-            <Link href="/">Home</Link>
-            <Link href="/dashboard">Dashboard</Link>
-            {session.user.name}
-            <img
-              src={session.user.image}
-              className="w-12 h-12 rounded-full"
-              alt="user image"
-            />
+          <div className="flex flex-row items-center gap-6 justify-center w-full relative py-2">
+            {/* <div className="flex-1"></div> */}
+            <div className="flex flex-row gap-6">
+              <Link
+                href="/"
+                className="hover:text-violet-400 border-b-2 border-transparent hover:border-violet-400"
+              >
+                Home
+              </Link>
+              <Link
+                href="/dashboard"
+                className="hover:text-violet-400 border-b-2 border-transparent hover:border-violet-400"
+              >
+                Dashboard
+              </Link>
+            </div>
+            <div className="flex flex-row items-center gap-6 absolute right-0">
+              <span>{session.user.name}</span>
+              <img
+                src={session.user.image}
+                className="w-12 h-12 rounded-full"
+                alt="user image"
+              />
+              <button
+                onClick={() => signOut({ callbackUrl: "/" })}
+                className="bg-black text-white rounded-md py-2 px-6 border-white border-2
+                hover:border-violet-700"
+              >
+                Sign out
+              </button>
+            </div>
           </div>
-          <button
-            onClick={() =>
-              signOut({ callbackUrl: "/" })
-            }
-            className="bg-black text-white rounded-md py-2 px-6 border-white border-2
-            hover:border-purple-700"
-          >
-            Sign out
-          </button>
         </>
       ) : (
         <button
           onClick={() => signIn()}
           className="bg-black text-white rounded-md px-6 py-2 border-2 border-white
-                hover:border-purple-700
+                hover:border-violet-700
             "
         >
           Sign in
